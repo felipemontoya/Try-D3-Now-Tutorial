bb362755799e3357e26527b1875d10cf4f260bb7
Felipe Montoya <felipe.montoya@edunext.co>
2013-10-28 18:27:47 -0500

    Binding data: Example with circles and a 1D array

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
+var dataset = [],
+    i = 0,
+    max = 5;
+    
+for(i=0; i<max; i++){
+    dataset.push(Math.round(Math.random()*100));
+}        
+
 var sampleSVG = d3.select("#viz")
     .append("svg")
-    .attr("width", 100)
-    .attr("height", 100);    
-
-sampleSVG.append("circle")
+    .attr("width", 80*max)
+    .attr("height", 100);
+    
+sampleSVG.selectAll("circle")
+    .data(dataset)
+    .enter().append("circle")
     .style("stroke", "gray")
     .style("fill", "white")
     .attr("r", 40)
-    .attr("cx", 50)
+    .attr("cx", function(d, i){return i*80+40})
     .attr("cy", 50)
     .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
     .on("mouseout", function(){d3.select(this).style("fill", "white");})
-    .on("mousedown", animate);
+    .on("mousedown", animateFirstStep);
 
-function animate() {
-    d3.select(this).transition()
+function animateFirstStep(){
+    d3.select(this)
+      .transition()
+        .delay(0)
         .duration(1000)
         .attr("r", 10)
+        .each("end", animateSecondStep);
+};
+
+function animateSecondStep(){
+    d3.select(this)
       .transition()
-        .delay(1000)
+        .duration(1000)
         .attr("r", 40);
 };
