474aaf9b9e9e7c4739d64513a5d16c0162f9f99b
Felipe Montoya <felipe.montoya@edunext.co>
2013-10-28 18:05:52 -0500

    Animation chaining: Using the event mousedown to trigger the animation function

--- js/main.js
+++ js/main.js
@@ -9,9 +9,15 @@ sampleSVG.append("circle")
     .attr("r", 40)
     .attr("cx", 50)
     .attr("cy", 50)
-  .transition()
-    .delay(100)
-    .duration(1000)    
-    .attr("r", 10)
-    .attr("cx", 30)
-    .style("fill", "black");
+    .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+    .on("mouseout", function(){d3.select(this).style("fill", "white");})
+    .on("mousedown", animate);
+
+function animate() {
+    d3.select(this).transition()
+        .duration(1000)
+        .attr("r", 10)
+      .transition()
+        .delay(1000)
+        .attr("r", 40);
+};
