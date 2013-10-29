0329fd6fd0e5d87380faa07975e2550038ad730c
Felipe Montoya <felipe.montoya@edunext.co>
2013-10-28 19:03:27 -0500

    Data Binding: Example with a table and a 2D array

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var dataset = [],
-    i = 0,
+    tmpDataset = [],
+    i, j,
     max = 5;
     
 for(i=0; i<max; i++){
@@ -38,3 +39,30 @@ function animateSecondStep(){
         .duration(1000)
         .attr("r", 40);
 };
+
+dataset = [];
+for (i = 0; i < 5; i++) {
+    for (j = 0, tmpDataset = []; j < 3; j++) {
+        tmpDataset.push("Row:"+i+",Col:"+j);
+    }
+    dataset.push(tmpDataset);
+}
+
+d3.select("#viz-2")
+    .append("table")
+    .style("border-collapse", "collapse")
+    .style("border", "2px black solid")
+    
+    .selectAll("tr")
+    .data(dataset)
+    .enter().append("tr")
+    
+    .selectAll("td")
+    .data(function(d){return d;})
+    .enter().append("td")
+    .style("border", "1px black solid")
+    .style("padding", "10px")
+    .on("mouseover", function(){d3.select(this).style("background-color", "aliceblue")}) 
+    .on("mouseout", function(){d3.select(this).style("background-color", "white")}) 
+    .text(function(d){return d;})
+    .style("font-size", "12px");
